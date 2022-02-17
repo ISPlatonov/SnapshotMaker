2943802f1d073936ed1f43382bb1b94c66355d87
isplatonov <isplatonov@miem.hse.ru>
2022-02-17 18:18:34 +0300

    minimal exception handling impl.

--- app/worker.py
+++ app/worker.py
@@ -7,13 +7,25 @@ max_snaps_number = 20
 
 
 def make_snapshot(addresses_list):
-    command = ['bash', 'app/scripts/make_snapshot.sh', camera_address]
-    process = subprocess.Popen(args=command, stdout=subprocess.PIPE)
-    filename = process.stdout.read()
-    
-    send_snapshot(filename, addresses_list)
+    if not isinstance(addresses_list, list):
+        raise TypeError("wrong datatype in request")
 
-    check_overlimit()
+    command = ['bash', 'app/scripts/make_snapshot.sh', camera_address]
+    try:
+        process = subprocess.Popen(args=command, stdout=subprocess.PIPE)
+        filename = process.stdout.read()
+    except Exception:
+        raise
+     
+    try:
+        send_snapshot(filename, addresses_list)
+    except Exception:
+        raise
+
+    try:
+        check_overlimit()
+    except Exception:
+        raise
     
 
 def send_snapshot(filename, address_list):
@@ -23,20 +35,29 @@ def send_snapshot(filename, address_list):
 
 def check_overlimit():
     command = ['bash', 'app/scripts/count_snapshots.sh']
-    process = subprocess.Popen(args=command, stdout=subprocess.PIPE)
-    snaps_number = int(process.stdout.read())
+    try:
+        process = subprocess.Popen(args=command, stdout=subprocess.PIPE)
+        snaps_number = int(process.stdout.read())
+    except Exception:
+        raise
 
     if snaps_number > max_snaps_number:
-        remove_overlimit(snaps_number - max_snaps_number)
+        try:
+            remove_overlimit(snaps_number - max_snaps_number)
+        except Exception:
+            raise
 
 
 def remove_overlimit(overlimit_number):
     # ... will write it later...
     command = ['bash', 'app/scripts/list_snapshots.sh']
-    process = subprocess.Popen(args=command, stdout=subprocess.PIPE)
-    files_list = process.stdout.read()
+    try:
+        process = subprocess.Popen(args=command, stdout=subprocess.PIPE)
+        files_list = process.stdout.read()
 
-    files_list = files_list.decode('utf-8').split()
+        files_list = files_list.decode('utf-8').split()
+    except Exception:
+        raise
 
     #print(overlimit_number)
 
